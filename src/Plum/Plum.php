0de472b3c3b57894ee1e879a9f2271ce48ed377d
Julien Brochet <mewt@madalynn.eu>
2012-04-09 01:18:16 +0200

    Welcome to 2012 (...)

--- src/Plum/Plum.php
+++ src/Plum/Plum.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Plum package.
  *
- * (c) 2010-2011 Julien Brochet <mewt@madalynn.eu>
+ * (c) 2010-2012 Julien Brochet <mewt@madalynn.eu>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
